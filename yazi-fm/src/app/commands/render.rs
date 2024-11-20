a41c90ed751e43837cec0cd3a1b7f0c229ad7d0a
三咲雅 · Misaki Masa <sxyazi@gmail.com>
2024-11-20 16:43:38 +0800

    feat: spotter (#1802)

--- yazi-fm/src/app/commands/render.rs
+++ yazi-fm/src/app/commands/render.rs
@@ -55,7 +55,7 @@ impl App {
 		let frame = term
 			.draw_partial(|f| {
 				_ = Lives::scope(&self.cx, || {
-					f.render_widget(crate::tasks::Progress, f.area());
+					f.render_widget(crate::tasks::Progress::new(&self.cx), f.area());
 					f.render_widget(crate::notify::Notify::new(&self.cx), f.area());
 					Ok(())
 				});
